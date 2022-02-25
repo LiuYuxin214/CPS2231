dc11c946c7f1a8a1945651b8b5bd7bb8fd006479
LiuYuxin214 <13791114010@163.com>
2022-02-25 17:22:58 +0800

    LiuYuxin

--- src/table.java
+++ src/table.java
@@ -10,6 +10,7 @@ public class table {
             for(int i=1;i<4;i++){
                 for(int j=1;j<4;j++){
                     if(i*j>2)
+
                         continue;
 
                     System.out.println(i*j);
