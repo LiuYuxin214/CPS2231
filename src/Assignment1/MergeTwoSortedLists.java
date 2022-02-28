4639388f8ad226370a40b7e8962c93d6f680e98b
LiuYuxin214 <13791114010@163.com>
2022-02-28 09:00:32 +0800

    LiuYuxin

--- src/Assignment1/MergeTwoSortedLists.java
+++ src/Assignment1/MergeTwoSortedLists.java
@@ -1,32 +1,32 @@
 package Assignment1;
 
-import java.util.*;
+import java.util.Scanner;
 
-//Purpose:
-//Main idea: 
+//Purpose: merges two sorted lists into a new sorted list
+//Main idea: array
 public class MergeTwoSortedLists {
 
-    public static int[] merge(int[] list1, int[] list2){
-        int size = list1[0]+list2[0];
-        int[] list=new int[size];
-        for(int i=1;i<list1[0];i++){
-            list[i]=list1[i];
+    public static int[] merge(int[] list1, int[] list2) {
+        int size = list1[0] + list2[0];
+        int[] list = new int[size];
+        for (int i = 1; i <= list1[0]; i++) {
+            list[i - 1] = list1[i];
         }
-        int j=1;
-        for(int i=list1[0];i<size;i++){
-            list[i]=list2[j];
+        int j = 1;
+        for (int i = list1[0]; i < size; i++) {
+            list[i] = list2[j];
             j++;
         }
-        int[] sort=new int[100000];
-        for(int i=0;i<size;i++){
+        int[] sort = new int[100000];
+        for (int i = 0; i < size; i++) {
             sort[list[i]]++;
         }
-        int[] result=new int[size+1];
-        j=1;
-        result[0]=size;
-        for(int i=0;i<100000;i++){
-            while(sort[i]!=0){
-                result[j]=i;
+        int[] result = new int[size + 1];
+        j = 1;
+        result[0] = size;
+        for (int i = 0; i < 100000; i++) {
+            while (sort[i] != 0) {
+                result[j] = i;
                 j++;
                 sort[i]--;
             }
@@ -36,37 +36,37 @@ public class MergeTwoSortedLists {
 
     public static void main(String[] args) {
         //1.
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter list1 size and contents: ");
-        int size1=input.nextInt();
-        int[] list1 = new int[size1+1];
-        list1[0]=size1;
-        for(int i=1;i<size1+1;i++){
-            list1[i]=input.nextInt();
+        int size1 = input.nextInt();
+        int[] list1 = new int[size1 + 1];
+        list1[0] = size1;
+        for (int i = 1; i < size1 + 1; i++) {
+            list1[i] = input.nextInt();
         }
         System.out.print("Enter list2 size and contents: ");
-        int size2=input.nextInt();
-        int[] list2 = new int[size2+1];
-        list2[0]=size2;
-        for(int i=1;i<size2+1;i++){
-            list2[i]=input.nextInt();
+        int size2 = input.nextInt();
+        int[] list2 = new int[size2 + 1];
+        list2[0] = size2;
+        for (int i = 1; i < size2 + 1; i++) {
+            list2[i] = input.nextInt();
         }
         //2.
         System.out.print("list1 is");
-        for(int i=1;i<size1+1;i++){
-            System.out.print(" "+list1[i]);
+        for (int i = 1; i < size1 + 1; i++) {
+            System.out.print(" " + list1[i]);
         }
         System.out.println();
         System.out.print("list2 is");
-        for(int i=1;i<size2+1;i++){
-            System.out.print(" "+list2[i]);
+        for (int i = 1; i < size2 + 1; i++) {
+            System.out.print(" " + list2[i]);
         }
         System.out.println();
         //3.
         System.out.print("The merged list is");
-        int[] result=merge(list1,list2);
-        for(int i=1;i<result[0]+1;i++){
-            System.out.print(" "+result[i]);
+        int[] result = merge(list1, list2);
+        for (int i = 1; i < result[0] + 1; i++) {
+            System.out.print(" " + result[i]);
         }
     }
 
