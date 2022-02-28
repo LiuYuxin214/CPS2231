d338553aab4de408c658d17ed86b3ef9e54d65c6
LiuYuxin214 <13791114010@163.com>
2022-02-28 12:28:52 +0800

    Liu Yuxin

--- src/Assignment1/Hangman.java
+++ src/Assignment1/Hangman.java
@@ -8,9 +8,9 @@ public class Hangman {
 
     public static int guess(String str) {
         Scanner input = new Scanner(System.in);
-        int count = 0, number = str.length();
-        char[] present = new char[number];
-        for (int i = 0; i < number; i++) {
+        int error = 0, size = str.length();
+        char[] present = new char[size];
+        for (int i = 0; i < size; i++) {
             present[i] = '*';
         }
         while (true) {
@@ -20,25 +20,24 @@ public class Hangman {
             }
             System.out.print(" > ");
             String letter = input.next();
-            int m = 0;
+            boolean hit = false;
             for (int i = 0; i < str.length(); i++) {
                 if (present[i] == letter.charAt(0)) {
                     System.out.println("     " + letter + " is already in the word");
-                    m = 1;
+                    hit = true;
                 } else if (str.charAt(i) == letter.charAt(0)) {
                     present[i] = letter.charAt(0);
-                    number--;
-                    m = 1;
+                    size--;
+                    hit = true;
                 }
             }
-            if (m == 0) {
+            if (hit == false) {
                 System.out.println("     " + letter + " is no in the word");
-                count++;
+                error++;
             }
-            if (number == 0)
-                break;
+            if (size == 0) break;
         }
-        return count;
+        return error;
     }
 
     public static void main(String[] args) {
@@ -47,8 +46,8 @@ public class Hangman {
             String[] words = {"write", "that", "program", "science", "computer"};
             int pos = (int) (Math.random() * 5);
             String str = words[pos];
-            int n = guess(str);
-            System.out.println("The word is " + str + ". You missed " + n + " time.");
+            int error = guess(str);
+            System.out.println("The word is " + str + ". You missed " + error + " time.");
             System.out.print("Do you want to guess for another word? Enter y or n>");
             String s = input.next();
             if (s.equals("n")) break;
