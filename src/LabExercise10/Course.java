eaae38c7527ed7c1b86eb2d2aa8d3de9eda41128
LiuYuxin214 <13791114010@163.com>
2022-04-05 11:47:03 +0800

    Liu Yuxin

---
+++ src/LabExercise10/Course.java
@@ -0,0 +1,37 @@
+package LabExercise10;
+
+public class Course {
+
+    private final String courseName;
+    private final String[] students = new String[100];
+    private int numberOfStudents;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void addStudent(String student) {
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+    }
+
+    public String[] getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+// Left as an exercise in Programming Exercise 10.9
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
