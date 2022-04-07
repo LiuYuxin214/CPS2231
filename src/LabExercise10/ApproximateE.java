fdf942950495016acc53a81568e22872d7525c46
LiuYuxin214 <13791114010@163.com>
2022-04-07 14:35:35 +0800

    Liu Yuxin

--- src/LabExercise10/ApproximateE.java
+++ src/LabExercise10/ApproximateE.java
@@ -1,7 +1,24 @@
 package LabExercise10;
 
-public class ApproximateE {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
 
+public class ApproximateE {
+    public static BigDecimal bigFactorial(BigDecimal bd) {
+        if (bd.compareTo(new BigDecimal(1)) == 0)
+            return bd;
+        else
+            return bd.multiply(bigFactorial(bd.subtract(new BigDecimal(1))));
+    }
+
+    public static void main(String[] args) {
+        for (int i = 100; i <= 1000; i += 100) {
+            BigDecimal e = new BigDecimal(1);
+            int j = 1;
+            while (j <= i) {
+                e = e.add(new BigDecimal(1).divide(bigFactorial(new BigDecimal(j)), 25, BigDecimal.ROUND_UP));
+                j++;
+            }
+            System.out.println("i = " + i + " e = " + e);
+        }
     }
 }
--- src/LabExercise10/LargePrimeNumbers.java
+++ src/LabExercise10/LargePrimeNumbers.java
@@ -1,7 +1,29 @@
 package LabExercise10;
 
+import java.math.BigDecimal;
+
 public class LargePrimeNumbers {
     public static void main(String[] args) {
+        int count = 0;
+        final long LONG_MAX = Long.MAX_VALUE;
+        String bigStart = LONG_MAX + "";
+        BigDecimal bigNum = new BigDecimal(bigStart);
+        while (count < 5) {
+            BigDecimal factor = new BigDecimal(2);
+            boolean isPrime = true;
+            while (bigNum.compareTo(factor) > 0) {
+                if (bigNum.remainder(factor).compareTo(new BigDecimal(0)) == 0) {
+                    isPrime = false;
+                    break;
+                }
+                factor = factor.add(new BigDecimal(1));
+            }
+            if (isPrime) {
+                System.out.println(bigNum + " is a prime number");
+                count++;
+            }
+            bigNum = bigNum.add(new BigDecimal(1));
 
+        }
     }
 }
