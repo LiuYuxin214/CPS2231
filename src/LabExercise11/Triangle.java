deeb3e0e7f5ad6fd06d283e8b22c35b271225972
LiuYuxin214 <13791114010@163.com>
2022-04-26 13:30:30 +0800

    Liu Yuxin

--- src/LabExercise11/Triangle.java
+++ src/LabExercise11/Triangle.java
@@ -1,53 +1,6 @@
 package LabExercise11;
 
-import java.util.Date;
 import java.util.Scanner;
-
-class GeometricObject {
-    private String color = "white";
-    private boolean filled;
-    private final Date dateCreated;
-
-    /**
-     * Construct a default geometric object
-     */
-    public GeometricObject() {
-        dateCreated = new Date();
-    }
-
-    /**
-     * Construct a geometric object with the specified color
-     * and filled value
-     */
-    public GeometricObject(String color, boolean filled) {
-        dateCreated = new Date();
-        this.color = color;
-        this.filled = filled;
-    }
-
-    /**
-     * Return color
-     */
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public boolean isFilled() {
-        return filled;
-    }
-
-    public void setFilled(boolean filled) {
-        this.filled = filled;
-    }
-
-    public Date getDateCreated() {
-        return dateCreated;
-    }
-}
 
 public class Triangle extends GeometricObject {
     private double side1 = 1.0;
