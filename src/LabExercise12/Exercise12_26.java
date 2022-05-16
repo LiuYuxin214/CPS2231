3a17057424067344ed408c5234df03eb6ec80dad
LiuYuxin214 <13791114010@163.com>
2022-05-16 21:15:09 +0800

    Liu Yuxin

--- src/LabExercise12/Exercise12_26.java
+++ src/LabExercise12/Exercise12_26.java
@@ -1,7 +1,19 @@
 package LabExercise12;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Exercise12_26 {
 
     public static void main(String[] args) {
+        System.out.print("Please enter the directory name: ");
+        Scanner input = new Scanner(System.in);
+        String dirName = input.nextLine();
+        File dir = new File(dirName);
+        if (dir.exists()) {
+            System.out.println("The directory exists.");
+        } else if (dir.mkdirs()) {
+            System.out.println("Directory created successfully");
+        }
     }
 }
