566db1e07066c03f8a2778aa161b4b0af26e9f42
LiuYuxin214 <13791114010@163.com>
2022-05-18 04:38:01 +0800

    Liu Yuxin

--- src/LabExercise13/Exercise13_04.java
+++ src/LabExercise13/Exercise13_04.java
@@ -3,6 +3,9 @@ package LabExercise13;
 import java.util.GregorianCalendar;
 
 public class Exercise13_04 {
+    /**
+     * Main method
+     */
     public static void main(String[] args) {
         GregorianCalendar calendar = new GregorianCalendar();
         if (args.length == 0) {
@@ -16,11 +19,20 @@ public class Exercise13_04 {
         }
     }
 
+    /**
+     * Print the calendar for a month in a year
+     */
     public static void printMonth(int year, int month) {
+        // Print the headings of the calendar
         printMonthTitle(year, month);
+
+        // Print the body of the calendar
         printMonthBody(year, month);
     }
 
+    /**
+     * Print the month title, e.g., May, 1999
+     */
     public static void printMonthTitle(int year, int month) {
         System.out.println("         " + getMonthName(month)
                 + " " + year);
@@ -28,7 +40,11 @@ public class Exercise13_04 {
         System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
     }
 
+    /**
+     * Get the English name for the month
+     */
     public static String getMonthName(int month) {
+
         return switch (month) {
             case 1 -> "January";
             case 2 -> "February";
@@ -46,9 +62,17 @@ public class Exercise13_04 {
         };
     }
 
+    /**
+     * Print month body
+     */
     public static void printMonthBody(int year, int month) {
+        // Get start day of the week for the first date in the month
         int startDay = getStartDay(year, month);
+
+        // Get number of days in the month
         int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
+
+        // Pad space before the first day of the month
         int i;
         for (i = 0; i < startDay; i++)
             System.out.print("    ");
@@ -63,25 +87,41 @@ public class Exercise13_04 {
         System.out.println();
     }
 
+    /**
+     * Get the start day of month/1/year
+     */
     public static int getStartDay(int year, int month) {
         final int START_DAY_FOR_JAN_1_1800 = 3;
+        // Get total number of days from 1/1/1800 to month/1/year
         int totalNumberOfDays = getTotalNumberOfDays(year, month);
+
+        // Return the start day for month/1/year
         return (totalNumberOfDays + START_DAY_FOR_JAN_1_1800) % 7;
     }
 
+    /**
+     * Get the total number of days since January 1, 1800
+     */
     public static int getTotalNumberOfDays(int year, int month) {
         int total = 0;
+
+        // Get the total days from 1800 to 1/1/year
         for (int i = 1800; i < year; i++)
             if (isLeapYear(i))
                 total = total + 366;
             else
                 total = total + 365;
+
+        // Add days from Jan to the month prior to the calendar month
         for (int i = 1; i < month; i++)
             total = total + getNumberOfDaysInMonth(year, i);
 
         return total;
     }
 
+    /**
+     * Get the number of days in a month
+     */
     public static int getNumberOfDaysInMonth(int year, int month) {
         if (month == 1 || month == 3 || month == 5 || month == 7 ||
                 month == 8 || month == 10 || month == 12)
@@ -92,9 +132,12 @@ public class Exercise13_04 {
 
         if (month == 2) return isLeapYear(year) ? 29 : 28;
 
-        return 0;
+        return 0; // If month is incorrect
     }
 
+    /**
+     * Determine if it is a leap year
+     */
     public static boolean isLeapYear(int year) {
         return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
